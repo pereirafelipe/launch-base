f3224a6dead4c14204154e59584bc8b306671c38
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-18 17:45:07 -0300

    Configure routes

--- Modulo3/server.js
+++ Modulo3/server.js
@@ -5,19 +5,23 @@ const server = express();
 
 server.use(express.static("public"));
 
-server.set("view engine", "html");
+server.use((req, res) => {
+  res.status(404).render("not-found");
+});
+
+server.set("view engine", "njk");
 
 nunjucks.configure("views", {
   express: server,
 });
 
 server.get("/", (req, res) => {
-  return res.render("index");
+  return res.render("about");
 });
 
-// server.get("/skills", (req, res) => {
-//   return res.render("skills");
-// });
+server.get("/courses", (req, res) => {
+  return res.render("courses");
+});
 
 server.listen(5000, () => {
   console.log("🚀 Server is running...");
