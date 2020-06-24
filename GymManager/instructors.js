d63ec8f5d8f1f78337ff41791671dfa9e2c1d05a
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-23 22:45:13 -0300

    Update function to create json

--- GymManager/instructors.js
+++ GymManager/instructors.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+const data = require("./data.json");
+
 exports.post = (req, res) => {
   const keys = Object.keys(req.body);
 
@@ -9,7 +11,9 @@ exports.post = (req, res) => {
     }
   });
 
-  fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+  data.instructors.push(req.body);
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Write file error!");
 
     return res.redirect("/instructors");
