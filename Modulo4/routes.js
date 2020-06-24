c129a289254d864ccdfb6afe45785030b1b6a026
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-24 20:39:01 -0300

    Create teachers register page

--- Modulo4/routes.js
+++ Modulo4/routes.js
@@ -10,6 +10,10 @@ routes.get("/teachers", (req, res) => {
   return res.render("teachers/index");
 });
 
+routes.get("/teachers/create", (req, res) => {
+  return res.render("teachers/create");
+});
+
 routes.get("/students", (req, res) => {
   return res.render("students");
 });
