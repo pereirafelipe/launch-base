724407d21079c9e38726a643a2852a2f153da9cc
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-04 14:20:47 -0300

    Config member model

--- GymManager2/src/app/controllers/members.js
+++ GymManager2/src/app/controllers/members.js
@@ -1,11 +1,14 @@
+const Member = require("../models/Member");
 const { handleAge, handleDate } = require("../../lib/utils");
 
 exports.index = (req, res) => {
-  return res.send("Index");
+  Member.all((members) => {
+    return res.render("members/index", { members });
+  });
 };
 
 exports.create = (req, res) => {
-  return res.send("Create");
+  return res.render("members/create");
 };
 
 exports.post = (req, res) => {
@@ -15,27 +18,63 @@ exports.post = (req, res) => {
     if (req.body[key] == "") return res.send("Please, fill all fields!");
   });
 
-  return res.send("Post");
+  Member.create(req.body, (member) => {
+    return res.redirect(`members/${member.id}`);
+  });
 };
 
 exports.show = (req, res) => {
-  return res.send("Show");
+  Member.find(req.params.id, (foundMember) => {
+    if (!foundMember) return res.send("Member not found!");
+
+    const birthDay = new Intl.DateTimeFormat("pt-BR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(foundMember.birth);
+
+    const member = {
+      ...foundMember,
+      age: handleDate(birthDay),
+    };
+
+    return res.render("members/show", { member });
+  });
 };
 
 exports.update = (req, res) => {
+  Member.find(req.params.id, (foundMember) => {
+    if (!foundMember) return res.send("Member not found!");
+
+    const date = new Intl.DateTimeFormat("pt-BR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(foundMember.birth);
+
+    const member = {
+      ...foundMember,
+      birth: date,
+    };
+
+    return res.render("members/edit", { member });
+  });
+};
+
+exports.put = (req, res) => {
   const keys = Object.keys(req.body);
 
   keys.map((key) => {
     if (req.body[key] == "") return res.send("Please, fill all fields!");
   });
 
-  return res.send("Update");
-};
-
-exports.put = (req, res) => {
-  return res.send("Put");
+  Member.update(req.body, () => {
+    return res.redirect(`members/${req.body.id}`);
+  });
 };
 
 exports.delete = (req, res) => {
-  return res.send("Delete");
+  Member.delete(req.body.id, () => {
+    return res.redirect(`/members`);
+  });
 };
---
+++ GymManager2/src/app/models/Member.js
@@ -0,0 +1,108 @@
+const db = require("../../config/db");
+
+module.exports = {
+  all(callback) {
+    db.query(`SELECT * FROM members`, (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows);
+    });
+  },
+  create(data, callback) {
+    let {
+      avatar_url,
+      name,
+      email,
+      birth,
+      gender,
+      blood,
+      weight,
+      height,
+    } = data;
+
+    const query = `
+    INSERT INTO members (
+      avatar_url,
+      name,
+      email,
+      birth,
+      gender,
+      blood,
+      weight,
+      height
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING id
+  `;
+
+    const values = [
+      avatar_url,
+      name,
+      email,
+      birth,
+      gender,
+      blood,
+      weight,
+      height,
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows[0]);
+    });
+  },
+  find(id, callback) {
+    db.query(`SELECT * FROM members WHERE id = $1`, [id], (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows[0]);
+    });
+  },
+  update(data, callback) {
+    let {
+      avatar_url,
+      name,
+      email,
+      birth,
+      gender,
+      blood,
+      weight,
+      height,
+    } = data;
+
+    const query = `
+      UPDATE members SET
+      avatar_url=($1),
+      name=($2),
+      birth=($3),
+      gender=($4),
+      services=($5)
+      WHERE id = $6
+    `;
+
+    const values = [
+      avatar_url,
+      name,
+      email,
+      birth,
+      gender,
+      blood,
+      weight,
+      height,
+      id,
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback();
+    });
+  },
+  delete(id, callback) {
+    db.query(`DELETE FROM members WHERE id = $1`, [id], (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback();
+    });
+  },
+};
