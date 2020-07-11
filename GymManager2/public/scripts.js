0d7f2ab0b3d4f7bc4bd4cc92a16545c37e374e78
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-10 23:27:50 -0300

    Create pagintion logic frontend

--- GymManager2/public/scripts.js
+++ GymManager2/public/scripts.js
@@ -6,3 +6,29 @@ for (const item of menuItems) {
     item.classList.add("active");
   }
 }
+
+let totalPages = 20,
+  selectedPage = 10,
+  oldPage,
+  pages = [];
+
+for (let currentPage = 0; currentPage <= totalPages; currentPage++) {
+  const firtAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firtAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...");
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+    pages.push(currentPage);
+
+    oldPage = currentPage;
+  }
+}
+
+console.log(pages);
