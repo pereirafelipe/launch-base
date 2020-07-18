cfe422c507c3fe7036927702898b30ba9840595c
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-18 16:24:24 -0300

    Update function

--- Modulo6/Funcoes/callback.js
+++ Modulo6/Funcoes/callback.js
@@ -1,11 +1,11 @@
-function setDoubleNumber(number, callback) {
-  return callback(number);
+function handleRandom(number, time) {
+  return setTimeout(() => console.log(number * 2), time);
 }
 
-function handleRandom(number) {
+function setDoubleNumber(number, callback) {
   const time = Math.floor(Math.random() * 100 + 1);
 
-  return setTimeout(() => console.log(number * 2), time);
+  callback(number, time);
 }
 
 setDoubleNumber(8, handleRandom);
