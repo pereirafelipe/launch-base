df06a5dee931bdd6eb7120a61772deb0543e1515
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-18 14:48:50 -0300

    Initial commit

---
+++ Modulo6/Modulo3/README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Iniciando no Back-end</h1>
+
+<h2 align="center">Desafio 3-1 ao 3-3: Página da Rocketseat</h2>
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/48728541/84934357-955dba80-b0ad-11ea-8364-0261c09d7aff.png" />
+</p>
+
+### Utilização do Nunjucks
+
+Nesses desafios foram utilizadas as mesmas páginas do desafio anterior, apenas refatorei a estrutura delas para obter dados dinâmicos com o nunjucks.
+
+
---
+++ Modulo6/Modulo3/data.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    id: "starter",
+    number: "01",
+    strong: "Torne-se um programador desejado ",
+    description: "no mercado com esses cursos gratuitos",
+    modules: "4 módulos | 80 aulas",
+    price: "Gratuito",
+  },
+  {
+    id: "launchbase",
+    number: "02",
+    strong: "Domine programação do zero ",
+    description: "e tenha acesso às melhores oportunidades do mercado",
+    modules: "7 módulos | 332 aulas",
+    price: "Pago",
+  },
+  {
+    id: "gostack",
+    number: "03",
+    strong: "Treinamento imersivo ",
+    description:
+      "nas tecnologias mais modernas de desenvolvimento web e mobile",
+    modules: "7 módulos | 332 aulas",
+    price: "Pago",
+  },
+];
