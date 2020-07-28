b93629c43b91f497f8b3460fd9a854273e174d5b
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-27 22:52:41 -0300

    Create search page

--- Launchstore/src/routes.js
+++ Launchstore/src/routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const ProductController = require("./app/controllers/ProductController");
 const HomeController = require("./app/controllers/HomeController");
+const SearchController = require("./app/controllers/SearchController");
+
 const multer = require("./app/middlewares/multer");
 
 const routes = express.Router();
 
 routes.get("/", HomeController.index);
+
+routes.get("/products/search", SearchController.index);
 
 routes.get("/products/create", ProductController.create);
 routes.get("/products/:id/edit", ProductController.edit);
