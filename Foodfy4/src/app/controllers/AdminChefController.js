ba4df8c70ae911aca3a1502ea09b63266f9b4940
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-28 22:02:51 -0300

    Create admin chefs controller

---
+++ Foodfy4/src/app/controllers/AdminChefController.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const data = require("../../data.json");
+
+module.exports = {
+  index(req, res) {
+    return res.render("admin/chefs/index");
+  },
+  show(req, res) {
+    return res.render("admin/chefs/show");
+  },
+  create(req, res) {
+    return res.render("admin/chefs/create");
+  },
+  post(req, res) {
+    return res.redirect("/admin/recipes");
+  },
+  edit(req, res) {
+    return res.render("admin/chefs/edit");
+  },
+  put(req, res) {
+    return res.redirect(`/admin/chefs/${index}`);
+  },
+  delete(req, res) {
+    return res.redirect(`/admin/recipes`);
+  },
+};
--- Foodfy4/src/routes.js
+++ Foodfy4/src/routes.js
@@ -4,6 +4,7 @@ const HomeController = require("./app/controllers/HomeController");
 const RecipeController = require("./app/controllers/RecipeController");
 const AdminRecipeController = require("./app/controllers/AdminRecipeController");
 const ChefController = require("./app/controllers/ChefController");
+const AdminChefController = require("./app/controllers/AdminChefController");
 
 const routes = express.Router();
 
@@ -23,104 +24,13 @@ routes.delete("/admin/recipes", AdminRecipeController.delete);
 
 routes.get("/chefs", ChefController.index);
 
-routes.get("/admin/chefs", (req, res) => {
-  return res.render("admin/chefs/index");
-});
-routes.get("/admin/chefs/create", (req, res) => {
-  return res.render("admin/chefs/create");
-});
-routes.get("/admin/chefs/:index", (req, res) => {
-  const recipeIndex = req.params.index;
-  const recipe = data.recipes[recipeIndex];
-
-  return res.render("admin/recipes/show", { recipe });
-});
-routes.get("/admin/chefs/:index/edit", (req, res) => {
-  const recipeIndex = req.params.index;
-  const tIndex = Number(recipeIndex) - 1;
-  const recipe = data.recipes[tIndex];
-
-  if (!recipe) return res.send("Recipe not found!");
-
-  return res.render("admin/recipes/edit", { recipe });
-});
-routes.post("/admin/chefs", (req, res) => {
-  const keys = Object.keys(req.body);
-  let {
-    image,
-    title,
-    author,
-    ingredients,
-    preparation,
-    information,
-  } = req.body;
-
-  keys.map((key) => {
-    if (req.body[key] === "") {
-      return res.send("Please, fill all fields!");
-    }
-  });
-
-  id = Number(data.recipes.length + 1);
-
-  data.recipes.push({
-    id,
-    image,
-    title,
-    author,
-    ingredients,
-    preparation,
-    information,
-  });
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect("/admin/recipes");
-  });
-});
-routes.put("/admin/chefs", (req, res) => {
-  const { id } = req.body;
-  let index = 0;
-
-  const foundRecipe = data.recipes.find((recipe, foundIndex) => {
-    if (recipe.id == id) {
-      index = foundIndex;
-      return true;
-    }
-  });
-
-  if (!foundRecipe) return res.send("Recipe not found!");
-
-  const recipe = {
-    ...foundRecipe,
-    ...req.body,
-    id: Number(req.body.id),
-  };
-
-  data.recipes[index] = recipe;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect(`/admin/recipes/${index}`);
-  });
-});
-routes.delete("/admin/chefs", (req, res) => {
-  const { id } = req.body;
-
-  const filteredRecipes = data.recipes.filter((recipe) => {
-    return recipe.id != id;
-  });
-
-  data.recipes = filteredRecipes;
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send("Write file error!");
-
-    return res.redirect(`/admin/recipes`);
-  });
-});
+routes.get("/admin/chefs", AdminChefController.index);
+routes.get("/admin/chefs/:index", AdminChefController.show);
+routes.get("/admin/chefs/create", AdminChefController.create);
+routes.post("/admin/chefs", AdminChefController.post);
+routes.get("/admin/chefs/:index/edit", AdminChefController.edit);
+routes.put("/admin/chefs", AdminChefController.put);
+routes.delete("/admin/chefs", AdminChefController.delete);
 
 routes.get("/results", (req, res) => {
   return res.render("user/results");
