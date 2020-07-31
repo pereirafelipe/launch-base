6b76597781e95ad07019730e9ec909675007f97a
Fernanda <fernandacarolineleite360@gmail.com>
2020-07-31 20:35:11 -0300

    Adds index and show chef controllers

--- Foodfy4/src/app/controllers/AdminChefController.js
+++ Foodfy4/src/app/controllers/AdminChefController.js
@@ -1,9 +1,27 @@
+const Chef = require("../models/Chef");
+const Recipe = require("../models/Recipe");
+
 module.exports = {
-  index(req, res) {
-    return res.render("admin/chefs/index");
+  async index(req, res) {
+    let results = await Chef.all();
+    const chefs = results.rows;
+
+    if (!chefs) return res.send("Recipes not found!");
+
+    return res.render("admin/chefs/index", { chefs });
   },
-  show(req, res) {
-    return res.render("admin/chefs/show");
+  async show(req, res) {
+    const { id } = req.params;
+
+    let results = await Chef.find(id);
+    const chef = results.rows[0];
+
+    if (!chef) return res.send("Chef not found!");
+
+    results = await Recipe.find(chef.id);
+    const recipes = results.rows;
+
+    return res.render("admin/chefs/show", { chef, recipes });
   },
   create(req, res) {
     return res.render("admin/chefs/create");
